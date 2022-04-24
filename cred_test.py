a1843c267e1fe3119c47b6976010fbbc3b1df5c5
Eva <rutheve.eva@gmail.com>
2022-04-24 10:16:28 +0300

    add function to test multiple pushes to the list

--- cred_test.py
+++ cred_test.py
@@ -13,7 +13,19 @@ class testCreds(unittest.TestCase):
     self.assertEqual(self.new_acc.account_username, "@ev")
     self.assertEqual(self.new_acc.account_password, "a1234")
 
-  # def test_save_credential(self):
+  
+  def tearDown(self):
+      Credentials.all_accounts = []
+
+  def test_save_credential(self):
+    '''
+    Func to test whether a new acc is added to the list
+    '''
+    
+    self.new_acc.add_credential()
+    another_acc = Credentials("Tumblr","eve","1234")
+    another_acc.add_credential()
+    self.assertEqual(len(Credentials.all_accounts),2)
     
 
 if __name__ == "__main__":
